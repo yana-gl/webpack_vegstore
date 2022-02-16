4feb870cf91231492b39da27a77d6ea272d99b22
yana_glushkova <yanaglushkova08@gmail.com>
2022-02-17 08:34:19 +1000

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.tsx",
   target: "web",
@@ -27,11 +26,8 @@ module.exports = {
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
-          // Creates `style` nodes from JS strings
           "style-loader",
-          // Translates CSS into CommonJS
           "css-loader",
-          // Compiles Sass to CSS
           "sass-loader",
         ],
         exclude: ["/macro-css"]
